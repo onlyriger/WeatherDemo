a07dcc9d4fd218fa889ec538e9d1b61a2a1b5910
ruanyizu <ruanyizu@diandianzu.com>
2016-06-14 15:09:42 +0800

    new class

---
+++ app/src/main/java/com/weather/app/btweather/model/Add.java
@@ -0,0 +1,10 @@
+package com.weather.app.btweather.model;
+
+/**
+ * Created by riger on 2016/6/14.
+ */
+public class Add {
+    public void out(){
+        System.out.println("new class out!");
+    }
+}

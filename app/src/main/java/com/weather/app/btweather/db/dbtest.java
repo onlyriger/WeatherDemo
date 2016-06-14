d84ed600b81ca0d96aabdec9f95bde332ede60a4
ruanyizu <ruanyizu@diandianzu.com>
2016-06-14 15:19:16 +0800

    comm class add

---
+++ app/src/main/java/com/weather/app/btweather/db/dbtest.java
@@ -0,0 +1,10 @@
+package com.weather.app.btweather.db;
+
+/**
+ * Created by riger on 2016/6/14.
+ */
+public class dbtest {
+    public void db(){
+        System.out.println("this is db");
+    }
+}
---
+++ app/src/main/java/com/weather/app/btweather/receiver/rectest.java
@@ -0,0 +1,10 @@
+package com.weather.app.btweather.receiver;
+
+/**
+ * Created by riger on 2016/6/14.
+ */
+public class rectest {
+    public void rec(){
+        System.out.println("this is rec");
+    }
+}
